116703d20c0ce6f0687d49e009c3dad686ba1b6c
sudecandan <sudecandnn@gmail.com>
2025-03-19 12:44:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -399,7 +399,7 @@ if st.button("🚀 Analizi Başlat"):
 col1 = st.columns(1)[0]  # Tek sütun kullan
 
 with col1:
-    seasonal_analysis_enabled = st.checkbox("##**Mevsimsel Dönem Analizi**", key="seasonal_analysis")
+    seasonal_analysis_enabled = st.checkbox("### **Mevsimsel Dönem Analizi**", key="seasonal_analysis")
 
 if seasonal_analysis_enabled:
     decrease_percentage_q = st.number_input("Q Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=30)
