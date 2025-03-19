5fe7ad469372d5308383f74510cdc9186ce4c42f
sudecandan <sudecandnn@gmail.com>
2025-03-19 11:27:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,17 +176,17 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.markdown("#### 📉 **P Analizi**")
-    decrease_percentage_p = st.number_input("📉 P Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=30)
+    decrease_percentage_p = st.number_input("P Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=30)
     decrease_count_p = st.number_input("🔄 P Kaç Kez Düşüş?", min_value=1, max_value=10, step=1, value=3)
 
 with col2:
     st.markdown("#### 📉 **T Analizi**")
-    decrease_percentage_t = st.number_input("📉 T Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=50)
+    decrease_percentage_t = st.number_input("T Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=50)
     decrease_count_t = st.number_input("🔄 T Kaç Kez Düşüş?", min_value=1, max_value=10, step=1, value=5)
 
 with col3 :
     st.markdown("#### 📉 **Q Analizi**")
-    decrease_percentage_q = st.number_input("📉 Q Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=30)
+    decrease_percentage_q = st.number_input("Q Yüzde Kaç Düşüş?", min_value=1, max_value=100, step=1, value=30)
 
 
 # **Seçili analizleri belirleme**
